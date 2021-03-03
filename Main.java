a974798c0bee19cae966679bd9d768d81e658c7c
Segev608 <57367786+Segev608@users.noreply.github.com>
2021-03-03 11:04:19 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+// ***** Identification Numbers *****
+// Segev Burstein, 322356155, Segev608@gmail.com
+// Yakir Demri, 206430167, yakird99@gmail.com
+
+
+import geometries.Sphere;
+import primitives.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static geometries.Intersectable.GeoPoint.convertPointsToGPoints;
+import static java.lang.System.out;
+import static org.junit.Assert.assertEquals;
+import static primitives.Util.*;
+
+import static geometries.Intersectable.GeoPoint;
+
+/**
+ * Test program for the 1st stage
+ * @author Dan Zilberstein
+ */
+public final class Main {
+
+	/**
+	 * Main program to tests initial functionality of the 1st stage
+	 * @param args irrelevant here
+	 */
+	public static void main(String[] args) {
+
+	}
+}
